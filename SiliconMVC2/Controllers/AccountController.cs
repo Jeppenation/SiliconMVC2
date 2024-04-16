ebdecd9386cf16b195b5ea080cab93a38bf1ae0a
Jesper Kallioniemi <jesper.kallioniemi@utb.ecutbildning.se>
2024-04-16 23:10:48 +0200

    Worked alot on the Details for account. Finally managed to make it save the information. Although, a bit different from Hans

--- SiliconMVC2/Controllers/AccountController.cs
+++ SiliconMVC2/Controllers/AccountController.cs
@@ -1,24 +1,137 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SiliconMVC2.ViewModels;
+using System.Security.Claims;
 
 namespace SiliconMVC2.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController(UserManager<UserEntity> userManager, AppDataContext context) : Controller
     {
-        public IActionResult Details()
+        private readonly UserManager<UserEntity> _userManager = userManager;
+        private readonly AppDataContext _context = context;
+
+        [Authorize]
+
+        public async Task<IActionResult> Details()
         {
-            return View();
+            var nameIdentifier = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+            var user = await _context.Users.Include(u => u.Address).FirstOrDefaultAsync(x => x.Id == nameIdentifier);
+
+            var model = new AccountDetailsViewModel 
+            { 
+                BasicInfo = new AccountBasicInfo
+                {
+                    FirstName = user!.FirstName,
+                    LastName = user.LastName,
+                    EmailAddress = user.Email!,
+                    Phone = user.PhoneNumber,
+                    Bio = user.Bio,
+                    ProfileImage = user.ProfilePicture
+                },
+                AddressInfo = new AccountAddressInfo
+                {
+                    AddressLine_1 = user.Address?.AddressLine1 ?? "",
+                    AddressLine_2 = user.Address?.AddressLine2,
+                    City = user.Address?.City ?? "",
+                    PostalCode = user.Address?.PostalCode ?? ""
+                }
+            }
+            ;
+            return View(model);
         }
 
         [HttpPost]
-        public async Task<IActionResult> UpdateBasicInfo()
+        public async Task<IActionResult> UpdateBasicInfo(AccountDetailsViewModel model)
         {
-            return View();
+            if (model.BasicInfo != null)
+            {
+                if (model.BasicInfo.FirstName != null && model.BasicInfo.LastName != null && model.BasicInfo.EmailAddress != null)
+                {
+                    var user = await _userManager.GetUserAsync(User);
+                    if (user != null)
+                    {
+                        user.FirstName = model.BasicInfo.FirstName;
+                        user.LastName = model.BasicInfo.LastName;
+                        user.Email = model.BasicInfo.EmailAddress;
+                        user.PhoneNumber = model.BasicInfo.Phone;
+                        user.Bio = model.BasicInfo.Bio;
+                        user.ProfilePicture = model.BasicInfo.ProfileImage;
+
+                        var result = await _userManager.UpdateAsync(user);
+                        if (result.Succeeded)
+                        {
+                            TempData["StatusMessage"] = "Basic info updated successfully.";
+                        }
+                        else
+                        {
+                            TempData["StatusMessage"] = "Basic info could not be updated.";
+                        }
+                    }
+                }
+                
+            }
+            else
+            {
+                TempData["StatusMessage"] = "Basic info could not be updated.";
+
+            }
+            return RedirectToAction("Details", "Account");
         }
 
         [HttpPost]
-        public async Task<IActionResult> UpdateAddressInfo()
+        public async Task<IActionResult> UpdateAddressInfo(AccountDetailsViewModel model)
         {
-            return View();
+            try
+            {
+                if (model.AddressInfo != null)
+                {
+                    if(model.AddressInfo.AddressLine_1 != null && model.AddressInfo.City != null && model.AddressInfo.PostalCode != null)
+                    {
+                        var user = await _userManager.GetUserAsync(User);
+                        if (user != null)
+                        {
+                            if (user.Address == null)
+                            {
+                                user.Address = new AddressEntity();
+                            }
+                            user.Address.AddressLine1 = model.AddressInfo.AddressLine_1;
+                            user.Address.AddressLine2 = model.AddressInfo.AddressLine_2;
+                            user.Address.City = model.AddressInfo.City;
+                            user.Address.PostalCode = model.AddressInfo.PostalCode;
+
+                            var result = await _userManager.UpdateAsync(user);
+                            if (result.Succeeded)
+                            {
+                                TempData["StatusMessage"] = "Address info updated successfully.";
+                            }
+                            else
+                            {
+                                TempData["StatusMessage"] = "Address info could not be updated.";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        TempData["StatusMessage"] = "Address info could not be updated.";
+                    }
+                    
+
+                }
+
+
+            }
+            catch
+            {
+                TempData["StatusMessage"] = "Address info could not be updated.";
+            }
+
+
+            
+            return RedirectToAction("Details", "Account");
         }
     }
 }
---
+++ SiliconMVC2/ViewModels/AccountDetailsViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SiliconMVC2.ViewModels
+{
+    public class AccountDetailsViewModel
+    {
+        public AccountBasicInfo BasicInfo { get; set; } = new AccountBasicInfo();
+        public AccountAddressInfo AddressInfo { get; set; } = new AccountAddressInfo();
+    }
+
+    public class AccountBasicInfo
+    {
+        public string userId { get; set; } = null!;
+
+        [DataType(DataType.ImageUrl)]
+        public string? ProfileImage { get; set; }
+
+        [Display(Name = "First Name", Prompt = "Enter your first name", Order = 0)]
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; } = null!;
+
+        [Display(Name = "Last Name", Prompt = "Enter your last name", Order = 1)]
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; } = null!;
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email address", Prompt = "Enter your email address", Order = 2)]
+        [Required(ErrorMessage = "Invalid email address")]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid email address")]
+        public string EmailAddress { get; set; } = null!;
+
+        [Display(Name = "Phone", Prompt = "Enter your phone", Order = 3)]
+        [DataType(DataType.PhoneNumber)]
+        public string? Phone { get; set; }
+
+        [Display(Name = "Bio", Prompt = "Add a short bio..", Order = 4)]
+        [DataType(DataType.MultilineText)]
+        public string? Bio { get; set; }
+    }
+
+
+    public class AccountAddressInfo
+    {
+        [Display(Name = "Address line 1", Prompt = "Enter your address line", Order = 0)]
+        [Required(ErrorMessage = "Address line is required")]
+        public string AddressLine_1 { get; set; } = null!;
+
+        [Display(Name = "Address line 2 (optional)", Prompt = "Enter your second address line", Order = 1)]
+        public string? AddressLine_2 { get; set; }
+
+        [Display(Name = "Postal code", Prompt = "Enter your postal code", Order = 2)]
+        [DataType(DataType.PostalCode)]
+        [Required(ErrorMessage = "Postal code is required")]
+        public string PostalCode { get; set; } = null!;
+
+        [Display(Name = "City", Prompt = "Enter your city", Order = 3)]
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; } = null!;
+    }
+}
